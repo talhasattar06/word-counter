f6326ffe4138b79847d275954c0479e7557defcc
Talha Sattar <160661345+talhasattar06@users.noreply.github.com>
2024-05-13 08:04:01 +0500

    pushed code

---
+++ index.js
@@ -0,0 +1,13 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+import chalk from "chalk";
+const answer = await inquirer.prompt([
+    {
+        name: "sentence",
+        type: "input",
+        message: chalk.hex("#6420AA")("Enter your sentence:")
+    }
+]);
+const sentence = answer.sentence.trim().split(" ");
+let words = sentence.length;
+console.log(`${chalk.hex("#97FFF4")("Your sentence word count is:")} ${chalk.hex("#FF7ED4")(words)}`);
